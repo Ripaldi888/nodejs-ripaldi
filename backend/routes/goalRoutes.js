08c37593b487daf8e4fc3d29ab17fc1da2812511
Ripaldi888 <muhripaldi07@gmail.com>
2024-08-28 18:15:26 +0700

    menambahkan method put

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const {getGoals, setGoals} = require('../controllers/goalController')
 
-router.get('/', (req,res) => {
-    res.status(200).json({
-        name:'ripaldi'
-    })
+// router.get('/', (req,res) => {
+//     res.status(200).json({
+//         name:'ripaldi'
+//     })
+// })
+
+// router.post('/', (req,res) => {
+//     res.status(200).json({
+//         name:'Ripaldi'
+//     })
+// })
+
+router.get('/', getGoals)
+router.post('/', setGoals)
+
+router.put('/',(req,res) => {
+    res.json({
+        message: "sudah terupdate"
+    })    
 })
 
 module.exports = router
